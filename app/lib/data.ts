887345eb0d0699ad9a3667198dd626752ea5eec4
dogancanokur <dogancanokur@gmail.com>
2024-01-28 21:33:40 +0300

    format code

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -159,15 +159,11 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `;
 
-    
-    console.log(data.rowCount);
-
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-    
 
     return invoice[0];
   } catch (error) {
