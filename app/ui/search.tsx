f15e0f30a8b1ecb0840a6330df85b3c5df1c6a3c
dogancanokur <dogancanokur@gmail.com>
2024-01-28 03:55:14 +0300

    pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,8 +12,8 @@ export default function Search({
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
